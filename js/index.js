80f2518804a130b43920799ab1a2a52217c6e499
Bryce Slade <bryceslade@gmail.com>
2021-09-27 13:46:07 -0600

    Working on main content

--- js/index.js
+++ js/index.js
@@ -46,6 +46,45 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
-let links = document.querySelector(".container nav");
-links.textContent = links["nav"]["nav-item-1"];
-// hello testing
+const links = document.querySelector("a");
+links.textContent = siteContent["nav"]["nav-item-1"];
+
+const link2 = links.nextElementSibling;
+link2.textContent = siteContent["nav"]["nav-item-2"];
+
+const link3 = link2.nextElementSibling;
+link3.textContent = siteContent["nav"]["nav-item-3"];
+
+const link4 = link3.nextElementSibling;
+link4.textContent = siteContent["nav"]["nav-item-4"];
+
+const link5 = link4.nextElementSibling;
+link5.textContent = siteContent["nav"]["nav-item-5"];
+
+const link6 = link5.nextElementSibling;
+link6.textContent = siteContent["nav"]["nav-item-6"];
+
+const headerTitle = document.querySelector(".cta-text h1");
+headerTitle.textContent = siteContent["cta"]["h1"];
+
+const headerButton = document.querySelector(".cta-text button");
+headerButton.textContent = siteContent["cta"]["button"];
+
+const headerPicture = document.querySelector("#cta-img");
+headerPicture.setAttribute("src", siteContent["cta"]["img-src"]);
+
+const topContent = document.querySelector(".top-content");
+const featuresHeader = topContent.querySelector(".text-content h4");
+featuresHeader.textContent = siteContent["main-content"]["features-h4"];
+
+const featuresPara = topContent.querySelector(".text-content p");
+featuresPara.textContent = siteContent["main-content"]["features-content"];
+
+const aboutHeader = topContent.nextElementSibling;
+aboutHeader.textConcent = siteContent["main-content"]["about-h4"];
+
+const contentPicture = document.querySelector("#middle-img");
+contentPicture.setAttribute(
+  "src",
+  siteContent["main-content"]["middle-img-src"]
+);
